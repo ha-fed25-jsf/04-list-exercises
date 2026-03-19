83f74360d517ef0a86e93f025417c5a035c82971
David Andersson <lejonmanen@gmail.com>
2026-03-19 10:19:53 +0100

    4.4b

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
+
 
 const CityList = ({ data }) => {
+	const [cities, setCities] = useState(data)
 
 	return (
 		<section>
 		<ul>
-			{data.map(city => (
-				<li key={city.id}> {city.name}, {city.population} invånare </li>
+			{cities.map(city => (
+				<li key={city.id}>
+					{city.name}, {city.population} invånare
+					<button onClick={() => setCities(cities.filter(c => c.id != city.id))}> Ta bort </button>
+				</li>
 			))}
 		</ul>
 		</section>
