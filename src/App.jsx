dd412cf997041cc0826170d295b195766a7215c7
David Andersson <lejonmanen@gmail.com>
2026-03-24 10:35:44 +0100

    5.1 Pronto

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import { useState } from 'react'
 import "./App.css";
 import ColorSettings from './components/ColorSettings.jsx'
 import CityList from './components/cityList/CityList.jsx';
+import Pronto from './components/pronto/Pronto.jsx'
 import { cities } from './data/cities.js';
 
-const COLORS = 'color-settings', CITIES = 'city-list'
+const COLORS = 'color-settings', CITIES = 'city-list', PRONTO = 'pronto'
 
 function App() {
-	const [view, setView] = useState(CITIES)
+	const [view, setView] = useState(PRONTO)
 
 	return (
 		<>
 			<header>
 				<button onClick={() => setView(COLORS)}> Favoritfärg + lista </button>
 				<button onClick={() => setView(CITIES)}> Lista med städer </button>
+				<button onClick={() => setView(PRONTO)}> Pronto </button>
 			</header>
 
 			{view === COLORS && <ColorSettings />}
 			{view === CITIES && <CityList data={cities} />}
+			{view === PRONTO && <Pronto />}
 		</>
 	);
 }
